51069055eed7c50b43e5bc7a5429f65710229cd5
adandotmosta <belarbiabdenour1@gmail.com>
2023-12-16 16:50:06 +0100

    beforr merging

---
+++ lib/presentation/sessionpercinema_screen/Sessions_tab_screen.dart
@@ -0,0 +1,166 @@
+
+import 'dart:ffi';
+
+import 'package:cinema_ticket_booking_app/core/app_export.dart';
+import 'package:cinema_ticket_booking_app/presentation/movie_sessions_page/movie_sessions_page.dart';
+import 'package:cinema_ticket_booking_app/presentation/details_page/details_page.dart';
+import 'package:cinema_ticket_booking_app/presentation/sessionpercinema_screen/sessionpercinema_screen.dart';
+import 'package:cinema_ticket_booking_app/widgets/app_bar/appbar_leading_image.dart';
+import 'package:cinema_ticket_booking_app/widgets/app_bar/appbar_title.dart';
+import 'package:cinema_ticket_booking_app/widgets/app_bar/custom_app_bar.dart';
+import 'package:cinema_ticket_booking_app/widgets/custom_bottom_bar.dart';
+import 'package:flutter/material.dart';
+
+class Sessions_tab_screen extends StatefulWidget {
+  const Sessions_tab_screen({Key? key}) : super(key: key);
+
+  @override
+  Sessions_tab_screenState createState() =>
+      Sessions_tab_screenState();
+}
+
+class Sessions_tab_screenState
+    extends State<Sessions_tab_screen>
+    with TickerProviderStateMixin {
+  late TabController tabviewController;
+  bool isAboutTabActive = true;
+
+  GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    tabviewController = TabController(length: 2, vsync: this);
+
+    // Listen for manual tab changes
+    tabviewController.addListener(() {
+      setState(() {
+        isAboutTabActive = tabviewController.index == 0;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    mediaQueryData = MediaQuery.of(context);
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: _buildAppBar(context),
+        body: SizedBox(
+          width: double.maxFinite,
+          child: Column(
+            children: [
+              _buildFixed(context),
+              Expanded(
+                child: SizedBox(
+                  height: 656.v,
+                  child: TabBarView(
+                    controller: tabviewController,
+                    children: [
+                      SessionpercinemaScreen(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: _buildBottomBar(context),
+      ),
+    );
+  }
+  Widget _buildFixed(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border.all(
+            color: Colors.transparent,
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(height: 10.h,),
+            Container(
+              height: 36.v,
+              width: 748.h,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isAboutTabActive = true;
+                          tabviewController.animateTo(0);
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          border: Border.all(
+                            color: Colors.transparent,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "About",
+                            style: TextStyle(
+                              color: isAboutTabActive ? Colors.red : Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10.h),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isAboutTabActive = false;
+                          tabviewController.animateTo(1);
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          border: Border.all(
+                            color: Colors.transparent,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Sessions",
+                            style: TextStyle(
+                              color: !isAboutTabActive ? Colors.red : Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return CustomAppBar(
+        leadingWidth: 56.h,
+        centerTitle: true,
+        title: AppbarTitle(text: "Sessions Available"));
+  }
+  Widget _buildBottomBar(BuildContext context) {
+    return CustomBottomBar(
+      onChanged: (BottomBarEnum type) {},
+    );
+  }
+}
